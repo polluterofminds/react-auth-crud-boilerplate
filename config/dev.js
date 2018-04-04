a7e635fd4b418f57ccb0a0401f6cb010044f454f
jehunter5811 <justin.edward.hunter@gmail.com>
2018-04-04 13:31:51 -0500

    Should have been part of gitignorefile

--- config/dev.js
+++
@@ -1,8 +0,0 @@
-//dev.js don't commit
-
-module.exports = {
-  googleClientID: '1002057797312-63aos1ttm84otuu6ak77atr8gt62g12u.apps.googleusercontent.com',
-  googleClientSecret: 'yvf2A8WX1RCdGLvkX7aXsOT1',
-  mongoURI: 'mongodb://justin:password@ds127436.mlab.com:27436/graphite-dev',
-  cookieKey: 'dhfakshfjkhfjhsajkhashiwoufu89uijnn'
-};
